e4cf472a866aed05fc4a3b26a82848c998f740ed
hj <parkhj062@gmail.com>
2025-12-18 23:12:29 +0900

    [문자 집합 조회]

---
+++ src/charset/AvailableCharsetMain.java
@@ -0,0 +1,30 @@
+package charset;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import java.util.SortedMap;
+
+public class AvailableCharsetMain {
+    public static void main(String[] args) {
+        SortedMap<String, Charset> charsets = Charset.availableCharsets();
+        for (String charsetName : charsets.keySet()) {
+            System.out.println("charsetName = " + charsetName);
+        }
+
+        System.out.println("====");
+        Charset charset1 = Charset.forName("MS949");
+        System.out.println("charset1 = " + charset1);
+
+        Set<String> aliases = charset1.aliases();
+        for (String alias : aliases) {
+            System.out.println("alias = " + alias);
+        }
+
+        Charset charset2 = StandardCharsets.UTF_8;
+        System.out.println("charset2 = " + charset2);
+
+        Charset defaultCharset = Charset.defaultCharset();
+        System.out.println("defaultCharset = " + defaultCharset);
+    }
+}
