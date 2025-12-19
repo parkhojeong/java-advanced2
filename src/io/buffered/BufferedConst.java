1d29375ab592fa6bf5eb6640798b8ea9e5b67334
hj <parkhj062@gmail.com>
2025-12-19 22:29:40 +0900

    [파일 입출력과 성능 최적화1 - 하나씩 쓰기]

---
+++ src/io/buffered/BufferedConst.java
@@ -0,0 +1,7 @@
+package io.buffered;
+
+public class BufferedConst {
+    public static final String FILE_NAME = "temp/buffered.txt";
+    public static final int FILE_SIZE = 10 * 1024 * 1024;
+    public static final int BUFFER_SIZE = 8192;
+}
