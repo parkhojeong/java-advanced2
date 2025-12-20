8fc1e061ed1a615f8568708ada22290fe9e467da
hj <parkhj062@gmail.com>
2025-12-20 18:32:22 +0900

    [30. 회원 관리 예제4 - ObjectStream]

--- src/io/member/MemberConsoleMain.java
+++ src/io/member/MemberConsoleMain.java
@@ -2,13 +2,14 @@ package io.member;
 
 import io.member.impl.DataMemberRepository;
 import io.member.impl.MemoryMemberRepository;
+import io.member.impl.ObjectMemberRepository;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class MemberConsoleMain {
 
-    private static final MemberRepository repository = new DataMemberRepository();
+    private static final MemberRepository repository = new ObjectMemberRepository();
 
     public static void main(String[] args) {
 
