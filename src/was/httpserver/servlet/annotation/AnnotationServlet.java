51b79e810282eb332c33e32b7a85e4c9c2dfbeb6
parkhojeong <parkhj062@gmail.com>
2025-12-24 01:43:19 +0900

    [97. HTTP 서버9 - 애노테이션 서블릿3 - 성능 최적화]
    [self-implementation]

--- src/was/httpserver/servlet/annotation/AnnotationServlet.java
+++ src/was/httpserver/servlet/annotation/AnnotationServlet.java
@@ -8,39 +8,49 @@ import was.httpserver.PageNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnnotationServlet implements HttpServlet {
 
     private final List<Object> controllers;
+    private final Map<String, Object[]> pathMap = new HashMap<>();
 
     public AnnotationServlet(List<Object> controllers) {
         this.controllers = controllers;
+
+        initPathMap(controllers);
+    }
+
+    private void initPathMap(List<Object> controllers) {
+        for (Object controller : controllers) {
+            Class<?> aClass = controller.getClass();
+            Method[] methods = aClass.getDeclaredMethods();
+            for (Method method : methods) {
+
+                Mapping mapping = method.getAnnotation(Mapping.class);
+                if(mapping == null) {
+                    continue;
+                }
+                if(pathMap.get(mapping.value()) != null) {
+                    throw new IllegalArgumentException("duplicate mapping path=" + mapping.value());
+                }
+                pathMap.put(mapping.value(), new Object[]{controller, method});
+            }
+        }
     }
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
         String path = request.getPath();
 
-        for (Object controller : controllers) {
-            Class<?> aClass = controller.getClass();
-
-
-            Method[] methods = aClass.getDeclaredMethods();
-            for (Method method : methods) {
-                Mapping mapping = method.getAnnotation(Mapping.class);
-                if (mapping == null) {
-                    continue;
-                }
-
-                if (path.equals(mapping.value())) {
-                    invoke(controller, method, request, response);
-                    return;
-                }
-            }
+        Object[] result = pathMap.get(path);
+        if(result == null) {
+            throw new PageNotFoundException("request=" + path);
         }
 
-        throw new PageNotFoundException("request=" + path);
+        invoke(result[0], (Method) result[1], request, response);
     }
 
     private static void invoke(Object controller, Method method, HttpRequest request, HttpResponse response) {
---
+++ src/was/v9/ServerMainV9.java
@@ -0,0 +1,27 @@
+package was.v9;
+
+import was.httpserver.HttpServer;
+import was.httpserver.HttpServlet;
+import was.httpserver.ServletManager;
+import was.httpserver.servlet.DiscardServlet;
+import was.httpserver.servlet.annotation.AnnotationServlet;
+import was.httpserver.servlet.annotation.AnnotationServletV2;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ServerMainV9 {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        List<Object> controllers = List.of(new SiteControllerV9(), new SearchControllerV9());
+        HttpServlet annotationServlet = new AnnotationServlet(controllers);
+
+        ServletManager servletManager = new ServletManager();
+        servletManager.setDefaultServlet(annotationServlet);
+        servletManager.add("/favicon.ico", new DiscardServlet());
+
+        HttpServer server = new HttpServer(PORT, servletManager);
+        server.start();
+    }
+}
